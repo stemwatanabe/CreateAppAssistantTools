d7dac693e2566d74a67a4a22b494f1fc56a241c7
渡辺 <e381bee3828ae38282@gmail.com>
2023-03-21 14:59:23 +0900

    add printer app

---
+++ PrintOnDisplay/PrintOnDisplay/PrintOnDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrintOnDisplayApp.swift
+//  PrintOnDisplay
+//
+//  Created by 渡辺幹 on 2023/03/21.
+//
+
+import SwiftUI
+
+@main
+struct PrintOnDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
