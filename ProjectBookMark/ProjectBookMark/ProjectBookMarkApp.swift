a06cf3241ad95c37b632d89ab976e99426b553de
渡辺 <e381bee3828ae38282@gmail.com>
2023-03-25 17:22:02 +0900

    add newproject

---
+++ ProjectBookMark/ProjectBookMark/ProjectBookMarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectBookMarkApp.swift
+//  ProjectBookMark
+//
+//  Created by 渡辺幹 on 2023/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ProjectBookMarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
